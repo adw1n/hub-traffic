ec77f99f1aae9983c6768e7ba52d8454205be3df
adw1n <adw1n@users.noreply.github.com>
2017-07-23 03:33:34 +0200

    Added scheduled task to download repo traffic stats from github every once in a while.

--- src/main/java/com/adw1n/hubtraffic/HubTrafficApplication.java
+++ src/main/java/com/adw1n/hubtraffic/HubTrafficApplication.java
@@ -4,6 +4,7 @@ import com.adw1n.hubtraffic.utils.GithubAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @SpringBootApplication
+@EnableScheduling
 public class HubTrafficApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HubTrafficApplication.class, args);
---
+++ src/main/java/com/adw1n/hubtraffic/scheduled/TrafficInfoUpdater.java
@@ -0,0 +1,30 @@
+package com.adw1n.hubtraffic.scheduled;
+
+import com.adw1n.hubtraffic.models.GithubUser;
+import com.adw1n.hubtraffic.respositories.GithubUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class TrafficInfoUpdater {
+    @Autowired
+    GithubUserRepository githubUserRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(TrafficInfoUpdater.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 6*60*60)
+    public void updateTrafficInfo() {
+        for(GithubUser user: githubUserRepository.findAll()){
+//            GithubAPI.fetchUpdates();
+            log.info("Updated traffic info for user {} at {}", user.getName(), dateFormat.format(new Date()));
+        }
+
+    }
+}
