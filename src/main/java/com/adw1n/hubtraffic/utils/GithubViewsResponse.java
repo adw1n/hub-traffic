5987a433aae8ef90d6a3178e394155db6b36b65a
adw1n <adw1n@users.noreply.github.com>
2017-07-30 20:30:10 +0200

    Refactor - got rid of constructors in favour of using Lambok annotations.

--- src/main/java/com/adw1n/hubtraffic/utils/GithubViewsResponse.java
+++ src/main/java/com/adw1n/hubtraffic/utils/GithubViewsResponse.java
@@ -1,23 +1,17 @@
 package com.adw1n.hubtraffic.utils;
 
 import com.adw1n.hubtraffic.models.GithubRepositoryViews;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GithubViewsResponse {
     private Integer count;
     private Integer uniques;
     private List<GithubRepositoryViews> views;
-
-
-    public GithubViewsResponse() {
-    }
-
-    public GithubViewsResponse(Integer count, Integer uniques, List<GithubRepositoryViews> views) {
-        this.count = count;
-        this.uniques = uniques;
-        this.views = views;
-    }
 }
